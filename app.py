a23b68de999a0d35b87023a1e7840ee3a8bab0db
yucong ma <78828665+yma54@users.noreply.github.com>
2022-04-22 23:18:50 -0500

    modification

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
 from flask import Flask
-from flask import render_template
+from flask import Flask,request,render_template,redirect,url_for
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Vertical Tank Maintainance')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About VTM')
+
+@app.route('/estimate', methods=['GET', 'POST'])
+def aestimate():
+    if request.method == 'POST':
+        form = request.form
+        radius=float(form['Radius'])
+        height=float(form['height'])
+        print(radius)
+        print(height)
+
+        pi=3.14
+        top=pi*radius**2
+        side=2*(pi*(radius*height))
+        area=top+side
+        sqft=area/144
+        part=sqft*25
+        labor=sqft*15
+        total=part+labor
+
+
+
+
+
+
+
+
+
+
+
+        return render_template('estimate.html',estimate=total)
+    return render_template('estimate.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
